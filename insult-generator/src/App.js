8d14d96ba7c3f7e1666ae0024073b2aeb0b1e860
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-03-30 08:59:39 -0700

    added more words to data.js

--- insult-generator/src/App.js
+++ insult-generator/src/App.js
@@ -6,15 +6,16 @@ import words from "./data";
 
 function App() {
   return (
+    
     <div className="App">
       <>
+      
         <img src={image} alt="old people at weddings meme" width="50%" />
-        <p>Call em' a</p>
-        <p>
-          {words[0].setOne[Math.floor(Math.random() * words[0].setOne.length)] +
-            " " +
-            words[1].setTwo[Math.floor(Math.random() * words[1].setTwo.length)]}
+        <h2>Call em' a</h2>
+        <p >
+          {words[0].setOne[Math.floor(Math.random() * words[0].setOne.length)] + " " + words[1].setTwo[Math.floor(Math.random() * words[1].setTwo.length)]}
         </p>
+      <button>new word</button>
       </>
     </div>
   );
