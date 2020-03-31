b3a6c0c985c4eae48367528755265ec83e1e40b2
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-03-31 10:42:12 -0700

    added netlify.toml

--- insult-generator/src/App.js
+++ insult-generator/src/App.js
@@ -1,24 +1,33 @@
+import React, { useState } from "react"
+import "./App.css"
+import image from "./callthema.png"
+import words from "./data"
 
-import React from "react";
-import "./App.css";
-import image from "./callthema.png";
-import words from "./data";
+const App = () => {
+  const [insult, setInsult] = useState(
+    words[0].setOne[Math.floor(Math.random() * words[0].setOne.length)] +
+      " " +
+      words[1].setTwo[Math.floor(Math.random() * words[1].setTwo.length)]
+  )
 
-function App() {
+  const randomizeInsult = () => {
+    setInsult(
+      words[0].setOne[Math.floor(Math.random() * words[0].setOne.length)] +
+        " " +
+        words[1].setTwo[Math.floor(Math.random() * words[1].setTwo.length)]
+    )
+  }
   return (
-    
     <div className="App">
       <>
-      
-        <img src={image} alt="old people at weddings meme" width="50%" />
+        <img src={image} alt="meme" width="50%" />
         <h2>Call em' a</h2>
-        <p >
-          {words[0].setOne[Math.floor(Math.random() * words[0].setOne.length)] + " " + words[1].setTwo[Math.floor(Math.random() * words[1].setTwo.length)]}
-        </p>
-      <button>new word</button>
+
+        <p className="randomInsultText">{insult}</p>
+        <button onClick={() => randomizeInsult()}>New Word</button>
       </>
     </div>
-  );
+  )
 }
 
 export default App;
